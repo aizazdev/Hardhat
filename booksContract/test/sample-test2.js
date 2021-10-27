85939818b4bc223bf5aca66641d9c49428fba8e8
Aizaz Ahmad <aizazkhan.uos@gmail.com>
2021-10-26 22:27:40 -0700

    Update sample-test2.js

--- booksContract/test/sample-test2.js
+++ booksContract/test/sample-test2.js
@@ -6,13 +6,28 @@ const TimeStampContract = require('../build/TimeStampContract.json');
 use(solidity);
 
 describe('TimeStampContract', () => {
-  const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-  const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/");
-  const signer = provider.getSigner();
- //0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266 
- it('Assigning intial cap token', async() => {
-  const Contract = await hre.ethers.getContractFactory("TimeStampContract");
-  const contract = await Contract.connect(signer).deploy(1000);
-  await contract.deployed;
+  let timeStamp;
+  beforeEach(async()=> {
+ 
+    const contractAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+    const provider = new ethers.providers.JsonRpcProvider("http://127.0.0.1:8545/");
+    const signer = provider.getSigner();
+  
+    const Contract = await hre.ethers.getContractFactory("TimeStampContract");
+    timeStamp = await Contract.connect(signer).deploy(1000);
+    await timeStamp.deployed;
+
+  });
+
+  it('Deploying Contract', async() => {
+    const name = await timeStamp.name();
+    expect(name).to.equal("Token");
+    const symbol = await timeStamp.symbol();
+    expect(symbol).to.equal("tk");
+  });
+
+  it("Capped Tokens", async() => {
+    const capToken = await timeStamp.duration();
+    console.log("capped token", capToken);
   });
 });
