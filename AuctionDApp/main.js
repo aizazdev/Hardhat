d7b696517320106f62f88ae974439d176aee8d33
Aizaz Ahmad <aizazkhan.uos@gmail.com>
2021-11-04 18:40:29 -0700

    Sale NFT
    Build NFT using hardhat this project can be useful as boiler plate as well

---
+++ AuctionDApp/main.js
@@ -0,0 +1,25 @@
+const serverUrl = "https://e2snjcb7nyzt.usemoralis.com:2053/server";
+const appId = "JWkzjIvKTSDGAi11MsQWtLuh9Gj1IlKzUxjHcba5";
+Moralis.start({ serverUrl, appId });
+
+/** Add from here down */
+async function login() {
+  let user = Moralis.User.current();
+  if (!user) {
+   try {
+      user = await Moralis.authenticate({ signingMessage: "Hello World!" })
+      console.log(user)
+      console.log(user.get('ethAddress'))
+   } catch(error) {
+     console.log(error)
+   }
+  }
+}
+
+async function logOut() {
+  await Moralis.User.logOut();
+  console.log("logged out");
+}
+
+document.getElementById("btn-login").onclick = login;
+document.getElementById("btn-logout").onclick = logOut;
